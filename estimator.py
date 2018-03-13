720672d6f2a5bc8912c3a35ccf42c8e49d4c8a54
Steve Farrell <sfarrell@cern.ch>
2018-03-12 17:13:19 -0700

    adding estimator pytorch code to gnn folder

---
+++ estimator.py
@@ -0,0 +1,94 @@
+"""
+This module contains the Estimator class implementation which provides
+code for doing the training of a PyTorch model.
+"""
+
+from __future__ import print_function
+
+from datetime import datetime
+from timeit import default_timer as timer
+
+import numpy as np
+
+import torch
+
+def logger(s):
+    """Simple logger function which prints date/time"""
+    print(datetime.now(), s)
+
+class Estimator():
+    """Estimator class"""
+    
+    def __init__(self, model, loss_func, opt='Adam',
+                 train_losses=None, valid_losses=None,
+                 cuda=False):
+        
+        self.model = model
+        if cuda:
+            self.model.cuda()
+        self.loss_func = loss_func
+        if opt == 'Adam':
+            self.optimizer = torch.optim.Adam(self.model.parameters())
+
+        self.train_losses = train_losses if train_losses is not None else []
+        self.valid_losses = valid_losses if valid_losses is not None else []
+        
+        logger('Model: \n%s' % model)
+        logger('Parameters: %i' %
+               sum(param.numel() for param in model.parameters()))
+    
+    def training_step(self, inputs, targets):
+        """Applies single optimization step on batch"""
+        self.model.zero_grad()
+        outputs = self.model(inputs)
+        loss = self.loss_func(outputs, targets)
+        loss.backward()
+        self.optimizer.step()
+        return loss
+    
+    def fit(self, train_input, train_target, batch_size=32, n_epochs=1,
+            valid_input=None, valid_target=None):
+        """Runs batch training for a number of specified epochs."""
+        if type(train_input) == list:
+            n_samples = train_input[0].size(0)
+        else:
+            n_samples = train_input.size(0)
+        n_batches = (n_samples + batch_size - 1) // batch_size
+        logger('Training samples: %i' % n_samples)
+        logger('Batches per epoch: %i' % n_batches)
+        if valid_input is not None:
+            n_valid = (valid_input[0].size(0) if type(valid_input) == list
+                       else valid_input.size(0))
+            logger('Validation samples: %i' % n_valid)
+
+        batch_idxs = np.arange(0, n_samples, batch_size)
+
+        epoch_start = len(self.train_losses)
+        epoch_end = epoch_start + n_epochs
+        for i in range(epoch_start, epoch_end):
+            logger('Epoch %i' % i)
+            start_time = timer()
+            sum_loss = 0
+
+            for j in batch_idxs:
+                # TODO: add support for more customized batching
+                if type(train_input) is list:
+                    batch_input = [ti[j:j+batch_size] for ti in train_input]
+                else:
+                    batch_input = train_input[j:j+batch_size]
+                batch_target = train_target[j:j+batch_size]
+                loss = self.training_step(batch_input, batch_target)
+                sum_loss += loss.cpu().data[0]
+
+            end_time = timer()
+            avg_loss = sum_loss / n_batches
+            self.train_losses.append(avg_loss)
+            logger('  training loss %.3g time %gs' %
+                   (avg_loss, (end_time - start_time)))
+            
+            # Evaluate the model on the validation set
+            if (valid_input is not None) and (valid_target is not None):
+                valid_loss = (self.loss_func(self.model(valid_input), valid_target)
+                              .cpu().data[0])
+                self.valid_losses.append(valid_loss)
+                logger('  validate loss %.3g' % valid_loss)
