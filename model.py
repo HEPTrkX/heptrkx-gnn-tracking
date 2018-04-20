2cca32420ee1ccd63e72b90078300315c70c08b6
Steve Farrell <sfarrell@cern.ch>
2018-04-19 21:34:39 -0700

    moved some model code into model module

---
+++ model.py
@@ -0,0 +1,92 @@
+"""
+This module implements the PyTorch modules that define the
+message-passing graph neural networks for hit or segment classification.
+"""
+
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+
+
+class EdgeNetwork(nn.Module):
+    """
+    A module which computes weights for edges of the graph.
+    For each edge, it selects the associated nodes' features
+    and applies some fully-connected network layers with a final
+    sigmoid activation.
+    """
+    def __init__(self, input_dim, hidden_dim=8, hidden_activation=nn.Tanh):
+        super(EdgeNetwork, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_dim*2, hidden_dim),
+            hidden_activation(),
+            nn.Linear(hidden_dim, 1),
+            nn.Sigmoid())
+    def forward(self, X, Ri, Ro):
+        # Select the features of the associated nodes
+        bo = torch.matmul(Ro.transpose(1, 2), X)
+        bi = torch.matmul(Ri.transpose(1, 2), X)
+        B = torch.cat([bo, bi], dim=2)
+        # Apply the network to each edge
+        return self.network(B).squeeze(-1)
+
+class NodeNetwork(nn.Module):
+    """
+    A module which computes new node features on the graph.
+    For each node, it aggregates the neighbor node features
+    (separately on the input and output side), and combines
+    them with the node's previous features in a fully-connected
+    network to compute the new features.
+    """
+    def __init__(self, input_dim, output_dim, hidden_activation=nn.Tanh):
+        super(NodeNetwork, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_dim*3, output_dim),
+            hidden_activation(),
+            nn.Linear(output_dim, output_dim),
+            hidden_activation())
+    def forward(self, X, e, Ri, Ro):
+        bo = torch.matmul(Ro.transpose(1, 2), X)
+        bi = torch.matmul(Ri.transpose(1, 2), X)
+        Rwo = Ro * e[:,None]
+        Rwi = Ri * e[:,None]
+        mi = torch.matmul(Rwi, bo)
+        mo = torch.matmul(Rwo, bi)
+        M = torch.cat([mi, mo, X], dim=2)
+        return self.network(M)
+
+class SegmentClassifier(nn.Module):
+    """
+    Segment classification graph neural network model.
+    Consists of an input network, an edge network, and a node network.
+    """
+    def __init__(self, input_dim=2, hidden_dim=8, n_iters=3, hidden_activation=nn.Tanh):
+        super(SegmentClassifier, self).__init__()
+        self.n_iters = n_iters
+        # Setup the input network
+        self.input_network = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            hidden_activation())
+        # Setup the edge network
+        self.edge_network = EdgeNetwork(input_dim+hidden_dim, hidden_dim, hidden_activation)
+        # Setup the node layers
+        self.node_network = NodeNetwork(input_dim+hidden_dim, hidden_dim, hidden_activation)
+
+    def forward(self, inputs):
+        """Apply forward pass of the model"""
+        X, Ri, Ro = inputs
+        # Apply input network to get hidden representation
+        H = self.input_network(X)
+        # Shortcut connect the inputs onto the hidden representation
+        H = torch.cat([H, X], dim=-1)
+        # Loop over iterations of edge and node networks
+        for i in range(self.n_iters):
+            # Apply edge network
+            e = self.edge_network(H, Ri, Ro)
+            # Apply node network
+            H = self.node_network(H, e, Ri, Ro)
+            # Shortcut connect the inputs onto the hidden representation
+            H = torch.cat([H, X], dim=-1)
+        # Apply final edge network
+        return self.edge_network(H, Ri, Ro)
