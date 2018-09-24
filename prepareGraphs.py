8e91a892eab69ebc4a91373e3ae2adb6c2c89b8d
Steve Farrell <sparticlesteve@gmail.com>
2018-09-23 22:33:29 -0700

    Added eta splitting in addition to phi splitting

--- prepareGraphs.py
+++ prepareGraphs.py
@@ -35,8 +35,12 @@ def parse_args():
     add_arg('--phi-slope-max', type=float, default=0.001,
             help='phi slope cut')
     add_arg('--z0-max', type=float, default=200, help='z0 cut')
-    add_arg('--n-phi-sectors', type=int, default=8,
-            help='Break detector into number of phi sectors')
+    add_arg('--n-phi-sections', type=int, default=8,
+            help='Break detector into number of phi sections')
+    add_arg('--n-eta-sections', type=int, default=2,
+            help='Break detector into number of eta sections')
+    add_arg('--eta-range', type=float, nargs=2, default=[-5, 5],
+            help='Considered range in eta')
     add_arg('--show-config', action='store_true',
             help='Dump the command line config')
     add_arg('--interactive', action='store_true',
@@ -73,21 +77,28 @@ def select_hits(hits, truth, particles, pt_min=0):
     ]
     return hits
 
-def split_phi_sectors(hits, n_phi_sectors=8):
-    phi_sector_width = 2 * np.pi / n_phi_sectors
-    phi_sector_edges = np.linspace(-np.pi, np.pi, n_phi_sectors + 1)
-    hits_sectors = []
-    # Loop over phi sectors
-    for i in range(n_phi_sectors):
-        phi_sector_min, phi_sector_max = phi_sector_edges[i:i+2]
-        # Select hits from this sector
-        sector_hits = hits[(hits.phi > phi_sector_min) & (hits.phi < phi_sector_max)]
-        # Center the phi sector on 0
-        centered_phi = sector_hits.phi - phi_sector_min - phi_sector_width / 2
-        sector_hits = sector_hits.assign(phi=centered_phi, phi_sector=i)
-        hits_sectors.append(sector_hits)
-    # Return results
-    return hits_sectors
+def calc_eta(r, z):
+    theta = np.arctan2(r, z)
+    return -1. * np.log(np.tan(theta / 2.))
+
+def split_detector_sections(hits, phi_edges, eta_edges):
+    """Split hits according to provided phi and eta boundaries."""
+    hits_sections = []
+    # Loop over sections
+    for i in range(len(phi_edges) - 1):
+        phi_min, phi_max = phi_edges[i], phi_edges[i+1]
+        # Select hits in this phi section
+        phi_hits = hits[(hits.phi > phi_min) & (hits.phi < phi_max)]
+        # Center these hits on phi=0
+        centered_phi = phi_hits.phi - (phi_min + phi_max) / 2
+        phi_hits = phi_hits.assign(phi=centered_phi, phi_section=i)
+        for j in range(len(eta_edges) - 1):
+            eta_min, eta_max = eta_edges[j], eta_edges[j+1]
+            # Select hits in this eta section
+            eta = calc_eta(phi_hits.r, phi_hits.z)
+            sec_hits = phi_hits[(eta > eta_min) & (eta < eta_max)]
+            hits_sections.append(sec_hits.assign(eta_section=j))
+    return hits_sections
 
 def print_hits_summary(hits):
     """Log some summary info of the hits DataFrame"""
@@ -99,7 +110,8 @@ def print_hits_summary(hits):
                  (n_events, n_hits, n_particles,
                   n_particles/n_events, n_hits/n_events))
 
-def process_event(prefix, pt_min, n_phi_sectors, phi_slope_max, z0_max):
+def process_event(prefix, pt_min, n_eta_sections, n_phi_sections,
+                  eta_range, phi_range, phi_slope_max, z0_max):
     # Load the data
     evtid = int(prefix[-9:])
     logging.info('Event %i, loading data' % evtid)
@@ -109,11 +121,17 @@ def process_event(prefix, pt_min, n_phi_sectors, phi_slope_max, z0_max):
     # Apply hit selection
     logging.info('Event %i, selecting hits' % evtid)
     hits = select_hits(hits, truth, particles, pt_min=pt_min).assign(evtid=evtid)
-    hits_sectors = split_phi_sectors(hits, n_phi_sectors=n_phi_sectors)
+
+    # Divide detector into sections
+    #phi_range = (-np.pi, np.pi)
+    phi_edges = np.linspace(*phi_range, num=n_phi_sections+1)
+    eta_edges = np.linspace(*eta_range, num=n_eta_sections+1)
+    hits_sections = split_detector_sections(hits, phi_edges, eta_edges)
+    #hits_sectors = split_phi_sectors(hits, n_phi_sectors=n_phi_sectors)
 
     # Graph features and scale
     feature_names = ['r', 'phi', 'z']
-    feature_scale = np.array([1000., np.pi / n_phi_sectors, 1000.])
+    feature_scale = np.array([1000., np.pi / n_phi_sections, 1000.])
 
     # Define adjacent layers
     n_det_layers = 10
@@ -122,11 +140,11 @@ def process_event(prefix, pt_min, n_phi_sectors, phi_slope_max, z0_max):
 
     # Construct the graph
     logging.info('Event %i, constructing graphs' % evtid)
-    graphs = [construct_graph(sector_hits, layer_pairs=layer_pairs,
+    graphs = [construct_graph(section_hits, layer_pairs=layer_pairs,
                               phi_slope_max=phi_slope_max, z0_max=z0_max,
                               feature_names=feature_names,
                               feature_scale=feature_scale)
-              for sector_hits in hits_sectors]
+              for section_hits in hits_sections]
 
     return graphs
 
@@ -155,7 +173,10 @@ def main():
     # Process input files with a worker pool
     with mp.Pool(processes=args.n_workers) as pool:
         process_func = partial(process_event, pt_min=args.pt_min,
-                               n_phi_sectors=args.n_phi_sectors,
+                               n_eta_sections=args.n_eta_sections,
+                               n_phi_sections=args.n_phi_sections,
+                               eta_range=args.eta_range,
+                               phi_range=(-np.pi, np.pi),
                                phi_slope_max=args.phi_slope_max,
                                z0_max=args.z0_max)
         graphs = pool.map(process_func, file_prefixes)
