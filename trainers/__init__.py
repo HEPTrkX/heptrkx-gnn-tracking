082a4bf46bc43033cab95631c2900cd18fb79c11
Steve Farrell <sparticlesteve@gmail.com>
2018-10-03 08:18:02 -0700

    added hello-world trainer class

---
+++ trainers/__init__.py
@@ -0,0 +1,16 @@
+"""
+Python module for holding our PyTorch trainers.
+
+Trainers here inherit from the BaseTrainer and implement the logic for
+constructing the model as well as training and evaluation.
+"""
+
+def get_trainer(name, **trainer_args):
+    """
+    Factory function for retrieving a trainer.
+    """
+    if name == 'hello':
+        from .hello import HelloTrainer
+        return HelloTrainer(**trainer_args)
+    else:
+        raise Exception('Trainer %s unknown' % name)
---
+++ trainers/hello.py
@@ -0,0 +1,54 @@
+"""
+Hello world PyTorch trainer.
+"""
+
+# System
+import time
+
+# Locals
+from .base_trainer import BaseTrainer
+
+class HelloTrainer(BaseTrainer):
+    """Hello world trainer object"""
+
+    def __init__(self, **kwargs):
+        super(HelloTrainer, self).__init__(**kwargs)
+
+    def build_model(self):
+        pass
+    
+    def print_model_summary(self):
+        self.logger.info('Hello world')
+
+    def write_checkpoint(self, checkpoint_id):
+        pass
+
+    def train_epoch(self, data_loader):
+        summary = dict()
+        start_time = time.time()
+        # Loop over training batches
+        for i, (batch_input, batch_target) in enumerate(data_loader):
+            batch_input = batch_input.to(self.device)
+            batch_target = batch_target.to(self.device)
+            self.logger.info('  Train batch input %s, target %s',
+                             batch_input.size(), batch_target.size())
+        self.logger.info('  Processed %i training batches' % (i + 1))
+        summary['train_time'] = time.time() - start_time
+        summary['train_loss'] = 0
+        return summary
+
+    def evaluate(self, data_loader):
+        """"Evaluate the model"""
+        summary = dict()
+        start_time = time.time()
+        # Loop over validation batches
+        for i, (batch_input, batch_target) in enumerate(data_loader):
+            batch_input = batch_input.to(self.device)
+            batch_target = batch_target.to(self.device)
+            self.logger.info('  Valid batch input %s, target %s',
+                             batch_input.size(), batch_target.size())
+        self.logger.info('  Processed %i validation batches' % (i + 1))
+        summary['valid_time'] = time.time() - start_time
+        summary['valid_loss'] = 0
+        summary['valid_acc'] = 1
+        return summary
