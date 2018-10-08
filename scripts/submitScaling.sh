61c53a7a9c19d022700f2277c0e8ac92537d7c0d
Steve Farrell <sparticlesteve@gmail.com>
2018-10-07 21:02:39 -0700

    added a submitScaling script

---
+++ scripts/submitScaling.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name=hello
+config=configs/${name}.yaml
+nodes="1 2 4 8 16"
+
+mkdir -p logs
+
+set -ex
+for n in $nodes; do
+    sbatch -J $name -N $n -d singleton -o "logs/$name-%j.out" \
+        scripts/batchScript.sh -d $config
+done
