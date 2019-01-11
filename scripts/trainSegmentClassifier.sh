4f406382b784c1d779910b029000cdfc9bb96a23
Steve Farrell <sparticlesteve@gmail.com>
2019-01-10 20:07:46 -0800

    update training script; log paths, big dataset, verbose printout

--- scripts/trainSegmentClassifier.sh
+++ scripts/trainSegmentClassifier.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#SBATCH -J segclf-big
 #SBATCH -C haswell
+#SBATCH -N 1
 #SBATCH -q debug
 #SBATCH -t 30
+#SBATCH -o logs/%x-%j.out
 
 . scripts/setup.sh
-srun -l python ./train.py -d configs/segclf.yaml
+srun -l python ./train.py -v -d configs/segclf_big.yaml
